03a06587161a8a75d4b31583a1f8967b44a49937
elliotmetcalf <103158470+elliotmetcalf@users.noreply.github.com>
2022-04-22 16:11:08 -0500

    Create LEDinterface.h

---
+++ code/LEDinterface.h
@@ -0,0 +1,12 @@
+/*
+ * LEDinterface.h
+ *
+ *  Created on: 18 Apr. 2022
+ *      Author: elliotmetcalf
+ */
+
+#ifndef LEDINTERFACE_H_
+#define LEDINTERFACE_H_
+void rgb_init_spi(void);
+void rgb_set_LEDs(const uint8_t *LED1,const uint8_t *LED2, const uint8_t *LED3, const uint8_t *LED4, const uint8_t *LED5, const uint8_t *LED6, const uint8_t *LED7, const uint8_t *LED8, const uint8_t *LED9, const uint8_t *LED10, const uint8_t *LED11,const uint8_t *LED12, const uint8_t *LED13, const uint8_t *LED14, const uint8_t *LED15, const uint8_t *LED16, const uint8_t *LED17, const uint8_t *LED18);
+#endif /* LEDINTERFACE_H_ */
