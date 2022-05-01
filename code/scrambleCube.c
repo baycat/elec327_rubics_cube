feb4e86e77adba9db7b66a2bc821212fa2ac0092
baycat <anyssa.cast@gmail.com>
2022-05-01 17:06:04 -0500

    base random code

--- code/scrambleCube.c
+++ code/scrambleCube.c
@@ -10,17 +10,51 @@
  * out: display faces for all faces
  */
 
+unsigned int seed_var;
+
+void srand(int seed) {
+    seed_var = (seed);
+}
+
+int rand(void) {
+    //linear feeback shift register as ~polynomial operation
+    //use seed to determine lfsr
+    //initialize variables
+    unsigned int lfsr = seed_var;
+    unsigned int bit;
+    unsigned int random_value = 0;
+    unsigned int i;
+
+    do {
+        for (i = 2; i > 0; i--) {
+            //polynomial implementation
+            bit = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
+            //taking mixed up back half
+            lfsr = (lfsr >> 1) | (bit << 15);
+
+            random_value = (random_value << 1) + bit;
+        }
+        seed_var = lfsr;
+        //for our 6 faces
+    } while (random_value > 5);
+    random_value++;
+
+    return random_value;
+}
 
 /*
  * Scramble cube
  *
- * - should we just have a bank of valid scrambles? im not sure how much memory that would take?
+ * - random value between 1 and 6 to choose which face moves
+ * 
+ * */
+
+
+/*
+ * - random value for potentiometer delta
  *
- * - how are we storing cube configurations dataframe wise?
  *
  *
- *
- * check for valid scramble
  *
  *
  *
