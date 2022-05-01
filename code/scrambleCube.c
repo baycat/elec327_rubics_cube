42a2047d60371da199bc948963865c4291021fb2
baycat <anyssa.cast@gmail.com>
2022-05-01 18:20:26 -0500

    set up rand for move parameters

--- code/scrambleCube.c
+++ code/scrambleCube.c
@@ -16,7 +16,34 @@ void srand(int seed) {
     seed_var = (seed);
 }
 
-int rand(void) {
+int f_rand(void) {
+    //linear feeback shift register as ~polynomial operation
+    //use seed to determine lfsr
+    //initialize variables
+    unsigned int lfsr = seed_var;
+    unsigned int bit;
+    unsigned int random_value = 0;
+    unsigned int i;
+
+    do {
+        for (i = 2; i > 0; i--) {
+            //polynomial implementation
+            bit = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
+            //taking mixed up back half
+            lfsr = (lfsr >> 1) | (bit << 15);
+
+            random_value = (random_value << 1) + bit;
+        }
+        seed_var = lfsr;
+        //for our 6 faces
+    } while (random_value > 5);
+    random_value++;
+
+    return random_value;
+}
+
+
+int m_rand(void) {
     //linear feeback shift register as ~polynomial operation
     //use seed to determine lfsr
     //initialize variables
@@ -49,14 +76,16 @@ int rand(void) {
  * 
  * */
 
-
 /*
  * - random value for potentiometer delta
  *
- *
- *
- *
- *
- *
  * display face
  */
+
+    for (i = 0; i < 20; i++) {
+        face = f_rand();
+        move = m_rand();
+
+        implement_move(face, move);
+
+    }
