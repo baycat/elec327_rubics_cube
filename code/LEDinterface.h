4e68e1e36edba0a50fdfcde0b17f9929c63813ac
elliotmetcalf <103158470+elliotmetcalf@users.noreply.github.com>
2022-05-04 23:02:06 -0500

    Update LEDinterface.h

--- code/LEDinterface.h
+++ code/LEDinterface.h
@@ -8,5 +8,10 @@
 #ifndef LEDINTERFACE_H_
 #define LEDINTERFACE_H_
 void rgb_init_spi(void);
-void rgb_set_LEDs(const uint8_t *LED1,const uint8_t *LED2, const uint8_t *LED3, const uint8_t *LED4, const uint8_t *LED5, const uint8_t *LED6, const uint8_t *LED7, const uint8_t *LED8, const uint8_t *LED9, const uint8_t *LED10, const uint8_t *LED11,const uint8_t *LED12, const uint8_t *LED13, const uint8_t *LED14, const uint8_t *LED15, const uint8_t *LED16, const uint8_t *LED17, const uint8_t *LED18);
+void rgb_set_LEDs(const uint8_t *LED1,const uint8_t *LED2, const uint8_t *LED3, const uint8_t *LED4, const uint8_t *LED5, const uint8_t *LED6, const uint8_t *LED7, const uint8_t *LED8, const uint8_t *LED9,
+                  const uint8_t *LED10, const uint8_t *LED11,const uint8_t *LED12, const uint8_t *LED13, const uint8_t *LED14, const uint8_t *LED15, const uint8_t *LED16, const uint8_t *LED17, const uint8_t *LED18,
+                  const uint8_t *LED19,const uint8_t *LED20, const uint8_t *LED21, const uint8_t *LED22, const uint8_t *LED23, const uint8_t *LED24, const uint8_t *LED25, const uint8_t *LED26, const uint8_t *LED27,
+                  const uint8_t *LED28, const uint8_t *LED29,const uint8_t *LED30, const uint8_t *LED31, const uint8_t *LED32, const uint8_t *LED33, const uint8_t *LED34, const uint8_t *LED35, const uint8_t *LED36,
+                  const uint8_t *LED37,const uint8_t *LED38, const uint8_t *LED39, const uint8_t *LED40, const uint8_t *LED41, const uint8_t *LED42, const uint8_t *LED43, const uint8_t *LED44, const uint8_t *LED45,
+                  const uint8_t *LED46, const uint8_t *LED47,const uint8_t *LED48, const uint8_t *LED49, const uint8_t *LED50, const uint8_t *LED51, const uint8_t *LED52, const uint8_t *LED53, const uint8_t *LED54);
 #endif /* LEDINTERFACE_H_ */
